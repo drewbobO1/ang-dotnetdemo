eacab26049e0893235d47cea49cc5b979e8c81c7
drewbob <122831351+drewbobO1@users.noreply.github.com>
2024-09-10 15:51:03 -0500

    add: Flesh out EF of initial PaymentDetail class

--- Models/PaymentDetail.cs
+++ Models/PaymentDetail.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace dotnetAPIdemo.Models;
 
@@ -7,11 +8,15 @@ public class PaymentDetail
     [Key]
     public int PaymentDetailId { get; set; }
 
-    public string CardOwnerName { get; set; }
+    [Column(TypeName = "nvarchar(100)")]
+    public string CardOwnerName { get; set; } = "";
 
-    public string CardNumber { get; set; }
+    [Column(TypeName = "nvarchar(16)")]
+    public string CardNumber { get; set; } = "";
     
-    public string ExpirationDate { get; set; }
+    [Column(TypeName = "nvarchar(5)")]
+    public string ExpirationDate { get; set; } = "";
     
-    public string SecurityCode { get; set; }
+    [Column(TypeName = "nvarchar(3)")]
+    public string SecurityCode { get; set; } = "";
 }
