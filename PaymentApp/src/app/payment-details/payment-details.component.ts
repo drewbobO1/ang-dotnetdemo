a802866e2dd468caa027f73d6dc301922be461b9
drewbob <122831351+drewbobO1@users.noreply.github.com>
2024-09-13 14:58:40 -0500

    feat: Add PaymentDetailService constructor to payment-detail-component.ts

--- PaymentApp/src/app/payment-details/payment-details.component.ts
+++ PaymentApp/src/app/payment-details/payment-details.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaymentDetailFormComponent } from './payment-detail-form/payment-detail-form.component';
+import { PaymentDetailService } from '../shared/payment-detail.service';
 
 @Component({
   selector: 'app-payment-details',
@@ -9,5 +10,7 @@ import { PaymentDetailFormComponent } from './payment-detail-form/payment-detail
   styles: ``
 })
 export class PaymentDetailsComponent {
-
+  constructor(public service: PaymentDetailService) {
+    
+  }
 }
