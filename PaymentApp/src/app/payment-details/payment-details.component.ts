60a9d1cabcf400fac0249b77a118afab498bf146
drewbob <122831351+drewbobO1@users.noreply.github.com>
2024-09-15 21:17:41 -0500

    feat: Add delete capabilites including confirmation dialog and toast notification

--- PaymentApp/src/app/payment-details/payment-details.component.ts
+++ PaymentApp/src/app/payment-details/payment-details.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { PaymentDetailFormComponent } from './payment-detail-form/payment-detail-form.component';
 import { PaymentDetailService } from '../shared/payment-detail.service';
 import { PaymentDetail } from '../shared/payment-detail.model';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-payment-details',
@@ -12,7 +13,7 @@ import { PaymentDetail } from '../shared/payment-detail.model';
   styles: ``
 })
 export class PaymentDetailsComponent implements OnInit {
-  constructor(public service: PaymentDetailService) {
+  constructor(public service: PaymentDetailService, private toastr: ToastrService) {
 
   }
   ngOnInit(): void {
@@ -21,5 +22,18 @@ export class PaymentDetailsComponent implements OnInit {
 
   populateForm(selectedRecord:PaymentDetail) {
     this.service.formData = Object.assign({}, selectedRecord);
+  }
+
+  onDelete(id: number) {
+    if (confirm("Are you sure you'd like to delete this card?")) {
+      this.service.deletePaymentDetail(id)
+      .subscribe({
+        next:res=>{
+          this.service.list = res as PaymentDetail[];
+          this.toastr.error("Card data deleted!", "Payment Detail Register");
+        },
+        error: err => {console.error(err);}
+      })
+    }
   }
 }
